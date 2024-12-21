be4d598999845b7cc702842776be35fe17baebc5
Roman <chidori031@mail.ru>
2024-12-21 17:11:36 +0300

    remove trash

--- main.py
+++ main.py
@@ -47,9 +47,8 @@ else:
 
 async def main():
     while True:
-        print("\nДействие:\n1. Показать список диалогов (узнать ID)\n2. Скачать последние сообщения из чата\n3. "
-              "Перезагрузить истекшие файлы (Error 400)\n4. Посмотреть участников группы\n5. Посмотреть каналы и "
-              "ботов\n0. Выход")
+        print("\nДействие:\n1. Показать список диалогов (узнать ID)\n2. Скачать последние сообщения из чата\n"
+              "3. Посмотреть участников группы\n4. Посмотреть каналы и ботов\n0. Выход")
 
         try:
             do = int(input("\nВведите номер действия: "))
@@ -61,36 +60,35 @@ async def main():
             print("Выход из программы...")
             break
         if do == 1:
-            limit = int(input("Введите количество чатов для получения: "))
+            limit = int(input("Введите количество чатов для получения (0 для всех): "))
             await get_dialogs(limit, "dialogs")
         elif do == 2:
             chat_id = input("Введите ID или username чата: ")
-            limit = int(input("Введите количество сообщений для получения: "))
+            limit = int(input("Введите количество сообщений для получения (0 для всех): "))
             await get_history(chat_id, limit)
         elif do == 3:
-            chat_id = input("Введите ID или username чата: ")
-            limit = int(input("Введите количество сообщений для получения: "))
-            folder_files = input("Введите полный путь к папке с поврежденными файлами: ")
-            await redownload_expired_files(chat_id, limit, folder_files)
+            chat_id = input("Введите ID группы: ")
+            limit = int(input("Введите количество участников для получения (0 для всех): "))
+            await get_chat_members_list(chat_id, limit)
         elif do == 4:
-            chat_id = input()
-            limit = int(input())
-        elif do == 5:
-            limit = int(input("Введите количество чатов для получения:"))
+            limit = int(input("Введите количество чатов для получения (0 для всех): "))
             await get_dialogs(limit, "other")
         else:
-            print("Ошибка: выберите действие от 0 до 5")
+            print("Ошибка: выберите действие от 0 до 4")
 
 
 async def get_dialogs(limit, search_type):
+    """Выводит список диалогов"""
     async with app:
         async for dialog in app.get_dialogs(limit=limit):
             if search_type == "dialogs":
                 if dialog.chat.type.name in ["PRIVATE", "GROUP", "SUPERGROUP"]:
-                    print(f"{dialog.chat.title or dialog.chat.first_name} (ID: {dialog.chat.id}) ({dialog.chat.type.name})")
+                    print(
+                        f"{dialog.chat.title or dialog.chat.first_name} (ID: {dialog.chat.id}) ({dialog.chat.type.name})")
             if search_type == "other":
                 if dialog.chat.type.name in ["CHANNEL", "BOT"]:
-                    print(f"{dialog.chat.title or dialog.chat.first_name} (ID: {dialog.chat.id}) ({dialog.chat.type.name})")
+                    print(
+                        f"{dialog.chat.title or dialog.chat.first_name} (ID: {dialog.chat.id}) ({dialog.chat.type.name})")
 
 
 async def get_history(chat_id, limit):
@@ -110,12 +108,10 @@ async def get_history(chat_id, limit):
         counter = 0
 
         # Сбор сообщений
-        async for message in app.get_chat_history(chat_id):
+        async for message in app.get_chat_history(chat_id, limit=limit):
             messages.append(message)
             counter += 1
             logging.info(f"Собрано: [{counter}/{limit}]")
-            if len(messages) >= limit:
-                break
 
         # Сохраняем до Х сообщения с конца
         # messages = messages[:15000]
@@ -196,63 +192,26 @@ async def get_history(chat_id, limit):
         print(f"Сообщения сохранены в файл: {file_name}")
 
 
-async def redownload_expired_files(chat_id, limit, expired_folder):
-    """Повторная загрузка файлов, названия которых есть в папке expired."""
-    async with app:  # Открываем клиент
-        # Получаем список файлов в папке expired
-        expired_files = os.listdir(expired_folder)
-        expired_files_set = set(expired_files)  # Для быстрого поиска
+# Функция для вывода участников чата
+async def get_chat_members_list(chat_id, limit):
+    """Выводит участников чата."""
+    async with app:
+        try:
+            print(f"Получение участников чата {chat_id}...")
+            members = []
 
-        logging.info(f"Найдено {len(expired_files)} файлов в папке expired. Начинаю повторное скачивание...")
-        await asyncio.sleep(1)
+            async for member in app.get_chat_members(chat_id, limit=limit):
+                members.append(member)
 
-        # Создаём папку для сохранения повторно скачанных файлов
-        redownload_folder = os.path.join(expired_folder, "redownloaded")
-        os.makedirs(redownload_folder, exist_ok=True)
+            # Вывод участников
+            print(f"Найдено {len(members)} участников:")
+            for member in members:
+                user = member.user
+                print(
+                    f"ID: {user.id}, Имя: {user.first_name or 'Неизвестное'} {user.last_name or ''}, Username: {user.username or 'Неизвестное'}")
 
-        messages = []
-        print("Собираю сообщения..")
-        counter = 0
-
-        async for message in app.get_chat_history(chat_id):
-            messages.append(message)
-            counter += 1
-            logging.info(f"Собрано: [{counter}/{limit}]")
-            if len(messages) >= limit:
-                break
-
-        counter = 0
-        # Итерируемся по сообщениям чата
-        for message in reversed(messages):
-            # Проверяем все медиа-файлы в сообщении
-            if message.voice or message.photo or message.video_note or message.video or message.document:
-                # Генерируем имя файла в соответствии с форматом
-                file_name = None
-                if message.voice:
-                    file_name = f"V_{message.date.strftime("%d_%m_%Y_%H%M%S")}_{message.from_user.username}.ogg"
-                elif message.photo:
-                    file_name = f"P_{message.date.strftime("%d_%m_%Y_%H%M%S")}_{message.from_user.username}.jpg"
-                elif message.video_note:
-                    file_name = f"VN_{message.date.strftime("%d_%m_%Y_%H%M%S")}_{message.from_user.username}.mp4"
-                elif message.video:
-                    file_name = f"VID_{message.date.strftime("%d_%m_%Y_%H%M%S")}_{message.from_user.username}.mp4"
-                elif message.document:
-                    file_name = f"DOC_{message.date.strftime("%d_%m_%Y_%H%M%S")}_{message.from_user.username}.pdf"
-
-                # Если имя файла найдено в списке expired_files
-                if file_name and file_name in expired_files_set:
-                    try:
-                        counter += 1
-                        logging.info(f"Обработка: [{counter}/{len(messages)}]")
-                        logging.info(f"Повторно скачиваю файл: {file_name}")
-                        new_file_path = os.path.join(redownload_folder, file_name)
-                        await message.download(file_name=new_file_path)
-                        logging.info(f"Файл успешно скачан: {file_name}")
-                    except Exception as e:
-                        logging.info(f"Ошибка при скачивании файла {file_name}: {e}")
-
-        logging.info(f"Повторное скачивание завершено. Файлы сохранены в папке: {redownload_folder}")
-        logging.info(f"Медиафайлы сохранены в папке: {redownload_folder}")
+        except Exception as e:
+            print(f"Ошибка при получении участников: {e}")
 
 
 app.run(main())
